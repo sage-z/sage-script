b07932ac817e65fa89c916d64cf0f2bafa049bc6
Sage <zhang@saget.cn>
2019-01-09 15:43:16 +0800

    分离项目

--- abc.md
+++
@@ -1,4 +0,0 @@
- isIdCard |  判断是否是中国身份证号码
- isBankId |  判断是否是中国银行账户
- isChineseOnly |  判断是否是全中文
- isEmail |  验证电子邮箱的格式
--- bin/sage.js
+++ bin/sage.js
@@ -29,57 +29,20 @@ if (process.argv.indexOf('-v') > -1) {
 }
 
 commander.version(pkg.version)
-  .option('-v --version', 'get version')
-  .option('-s --silent', 'hide all messages', false)
-  .option('-m --mini-list', 'display a compacted list without formatting')
-  .option('-f --force', 'force actions')
-  .option('--disable-logs', 'do not write logs')
+  .option('-v --version', '获取版本号')
   .option('-n --name <name>', 'set a <name> for script')
-  .option('-i --instances <number>', 'launch [number] instances (for networked app)(load balanced)')
-  .option('--parallel <number>', 'number of parallel actions (for restart/reload)')
   .option('-l --log [path]', 'specify entire log file (error and out are both included)')
   .option('-o --output <path>', 'specify out log file')
   .option('-e --error <path>', 'specify error log file')
   .option('-p --pid <pid>', 'specify pid file')
   .option('-k --kill-timeout <delay>', 'delay before sending final SIGKILL signal to process')
-  .option('--listen-timeout <delay>', 'listen timeout on application reload')
-  .option('--max-memory-restart <memory>', 'specify max memory amount used to autorestart (in octet or use syntax like 100M)')
-  .option('--restart-delay <delay>', 'specify a delay between restarts (in milliseconds)')
   .option('--env <environment_name>', 'specify environment to get specific env variables (for JSON declaration)')
-  .option('--log-type <type>', 'specify log output style (raw by default, json optional)')
-  .option('-x --execute-command', 'execute a program using fork system')
-  .option('--max-restarts [count]', 'only restart the script COUNT times')
   .option('-u --user <username>', 'define user when generating startup script')
   .option('--uid <uid>', 'run target script with <uid> rights')
   .option('--gid <gid>', 'run target script with <gid> rights')
   .option('--cwd <path>', 'run target script as <username>')
-  .option('--hp <home path>', 'define home path when generating startup script')
-  .option('--service-name <name>', 'define service name when generating startup script')
   .option('-c --cron <cron_pattern>', 'restart a running process based on a cron pattern')
-  .option('-w --write', 'write configuration in local folder')
-  .option('--interpreter-args <arguments>', 'interpret options (alias of --node-args)')
-  .option('--log-date-format <date format>', 'add custom prefix timestamp to logs')
-  .option('-a --update-env', 'update environment on restart/reload (-a <=> apply)')
-  .option('--source-map-support', 'force source map support')
-  .option('--only <application-name>', 'with json declaration, allow to only act on one application')
-  .option('--disable-source-map-support', 'force source map support')
-  .option('--merge-logs', 'merge logs from different instances but keep error and out separated')
   .option('--watch [paths]', 'watch application folder for changes', function(v, m) { m.push(v); return m;}, [])
-  .option('--ignore-watch <folders|files>', 'folder/files to be ignored watching, should be a specific name or regex - e.g. --ignore-watch="test node_modules \"some scripts\""')
-  .option('--node-args <node_args>', 'space delimited arguments to pass to node in cluster mode - e.g. --node-args="--debug=7001 --trace-deprecation"')
-  .option('--no-color', 'skip colors')
-  .option('--no-vizion', 'start an app without vizion feature (versioning control)')
-  .option('--no-autorestart', 'start an app without automatic restart')
-  .option('--no-treekill', 'Only kill the main process, not detached children')
-  .option('--no-pmx', 'start an app without pmx')
-  .option('--no-automation', 'start an app without pmx')
-  .option('--trace', 'enable transaction tracing with km')
-  .option('--disable-trace', 'disable transaction tracing with km')
-  .option('--attach', 'attach logging after your start/restart/stop/reload')
-  .option('--sort <field_name:sort>', 'sort process according to field\'s name')
-  .option('--v8', 'enable v8 data collecting')
-  .option('--event-loop-inspector', 'enable event-loop-inspector dump in pmx')
-  .option('--deep-monitoring', 'enable all monitoring tools (equivalent to --v8 --event-loop-inspector --trace)')
   .usage('[cmd] app');
 
 commander.on('--help', function() {
@@ -109,8 +72,8 @@ if (_arr.indexOf('--no-daemon') > -1) {
   //
   // Start daemon if it does not exist
   //
-  // Function checks if --no-daemon option is present,
-  // and starts daemon in the same process if it does not exists
+  // 检查是否有 --no-daemon 选项,
+  // 如果守护进程不存在，则在同一进程中启动它
   //
   console.log('pm2 launched in no-daemon mode (you can add DEBUG="*" env variable to get more messages)');
 
@@ -125,7 +88,7 @@ else {
 }
 
 //
-// Helper function to fail when unknown command arguments are passed
+// 传递未知命令参数时，Helper函数失败
 //
 function failOnUnknown(fn) {
   return function(arg) {
@@ -167,7 +130,7 @@ commander.command('start <file|json|stdin|app_name|pm_id...>')
   .option('--dockerdaemon', 'for debugging purpose')
   .description('start and daemonize an app')
   .action(function(cmd, opts) {
-    
+    console.log('asdf')
   });
 
 commander.command('trigger <proc_name> <action_name> [params]')
@@ -233,112 +196,6 @@ commander.command('update')
   .action(function() {
     pm2.update();
   });
-
-/**
- * Module specifics
- */
-commander.command('install [module|git:// url|json]')
-  .alias('module:install')
-  .option('--v1', 'install module in v1 manner (do not use it)')
-  .option('--safe [time]', 'keep module backup, if new module fail = restore with previous')
-  .description('install or update a module (or a set of modules) and run it forever')
-  .action(function(plugin_name, opts) {
-    if (opts.v1)
-      commander.v1 = true;
-    if (opts.safe)
-      commander.safe = opts.safe;
-  });
-
-commander.command('uninstall <module>')
-  .alias('module:uninstall')
-  .description('stop and uninstall a module')
-  .action(function(plugin_name) {
-  });
-
-
-commander.command('publish')
-  .alias('module:publish')
-  .description('Publish the module you are currently on')
-  .action(function() {
-  });
-
-commander.command('set [key] [value]')
-  .description('sets the specified config <key> <value>')
-  .action(function(key, value) {
-  });
-
-commander.command('get [key]')
-  .description('get value for <key>')
-  .action(function(key) {
-  });
-
-commander.command('conf [key] [value]')
-  .description('get / set module config values')
-  .action(function(key, value) {
-  });
-
-commander.command('config <key> [value]')
-  .description('get / set module config values')
-  .action(function(key, value) {
-  });
-
-commander.command('unset <key>')
-  .description('clears the specified config <key>')
-  .action(function(key) {
-  });
-
-//
-// Keymetrics CLI integratio
-//
-commander.command('link [secret] [public] [name]')
-  .alias('interact')
-  .option('--info-node [url]', 'set url info node')
-  .description('linking action to keymetrics.io - command can be stop|info|delete|restart')
-  .action(function() {
-    
-  });
-
-commander.command('unlink')
-  .description('linking action to keymetrics.io - command can be stop|info|delete|restart')
-  .action(function() {
-    
-  });
-
-
-commander.command('open')
-  .description('open dashboard in browser')
-  .action(function(name) {
-  });
-
-commander.command('register')
-  .description('create an account on keymetrics')
-  .action(function(name) {
-  });
-
-commander.command('login')
-  .description('login to keymetrics and link current PM2')
-  .action(function(name) {
-  });
-
-//
-// Web interface
-//
-commander.command('web')
-  .description('launch a health API on ' + cst.WEB_IPADDR + ':' + cst.WEB_PORT)
-  .action(function() {
-    console.log('Launching web interface on ' + cst.WEB_IPADDR + ':' + cst.WEB_PORT);
-
-  });
-
-//
-// Save processes to file
-//
-commander.command('dump')
-  .alias('save')
-  .description('dump all processes for resurrecting them later')
-  .action(failOnUnknown(function() {
-    
-  }));
 
 //
 // Save processes to file
@@ -459,7 +316,7 @@ commander.command('*')
   .action(function() {
     console.log(cst.PREFIX_MSG + '\nCommand not found');
     commander.outputHelp();
-    // Check if it does not forget to close fds from RPC
+    // 检查是否忘记关闭RPC的fds
     process.exit(cst.ERROR_EXIT);
   });
 
@@ -469,6 +326,6 @@ commander.command('*')
 if (process.argv.length == 2) {
   commander.parse(process.argv);
   commander.outputHelp();
-  // Check if it does not forget to close fds from RPC
+  // 检查是否忘记关闭RPC的fds
   process.exit(cst.ERROR_EXIT);
 }
